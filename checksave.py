946658584ef062b59ab5cdaf91a55932838b1250
80aX <avkids@yandex.ru>
2017-02-04 23:08:10 +0300

    deleted checksave.py

--- checksave.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime, timedelta
-
-def CheckSave():
-    Time = datetime.now() - timedelta(0, 30)
-    if InJournalBetweenTimes('Сохранение мира.', Time, datetime.now()) >= 0:
-        Wait(10000)
