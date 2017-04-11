56c8d3579aa3157e77d7ce55e30cb55dd0ae63b1
80aX <avkids@yandex.ru>
2017-04-11 13:53:19 +0300

    simple tracking script

---
+++ tracking.py
@@ -0,0 +1,24 @@
+from all import Hungry, CheckSave
+
+
+def tracking():
+	CancelMenu()
+	WaitMenu('category', '(0)')
+	WaitMenu('creature', '(0)')
+	UseSkill('Tracking')
+	return
+
+
+def main():
+	SetARStatus(True)
+	while Connected and (not Dead()):
+		Hungry()
+		for i in range(100):
+			CheckSave()
+			tracking()
+			Wait(10000)
+	return
+
+
+if __name__ == "__main__":
+	main()
